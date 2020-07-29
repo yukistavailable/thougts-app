f1a541667df73007e76ada2145f0e088f5672c3a
yukistavailable <yukitatsu_0817@yahoo.co.jp>
2020-07-29 21:23:56 +0900

    define db model

--- api/main.go
+++ api/main.go
@@ -7,6 +7,31 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+type User struct {
+	gorm.Model
+	UserName string `gorm:"unique;not null"`
+	Thoughts []Thought `gorm:"foreignkey:UserID"`
+}
+
+type UserProfile struct {
+	Sentences string
+	User User `gorm:"foreignkey:UserID"`
+	UserID uint
+	Follows []int
+	Followers []int
+}
+
+type Thought struct {
+	gorm.Model
+	UserID uint
+	ParentThoughtID uint `gorm:"default:0"`
+	IsTheme bool `gorm:"default:false"`
+	Content string
+	LikeUsers []int
+}
+
+
+
 func main() {
 	db, err := gorm.Open("postgres", "host=db user=app_user dbname=app_db password=postgres_password sslmode=disable port=5432")
 	defer db.Close()
